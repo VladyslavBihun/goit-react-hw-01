6a8adcabd4e10e3445705bb61e25b76f83914de7
Vladyslav Bihun <bihunvv@gmail.com>
2024-05-14 19:19:22 +0300

    second task done without styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import Profile from "./components/Profile/Profile"
-import data from './userData.json'
-
+import Profile from "./components/Profile/Profile";
+import data from "./userData.json";
+import FriendList from "./components/FriendList/FriendList";
+import friends from "./friends.json";
 
 function App() {
   return (
@@ -12,8 +13,9 @@ function App() {
         avatar={data.avatar}
         stats={data.stats}
       />
+      <FriendList friends={friends} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
